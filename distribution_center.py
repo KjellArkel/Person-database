c129710a5fb34bddc9dea51e9ebd63bf89ed62b7
Pannakoek <ke-man@live.nl>
2017-11-16 22:56:58 +0100

    expand the birthday question and at ToDo comments
    First do the ToDo in functions.py

--- distribution_center.py
+++ distribution_center.py
@@ -1,36 +1,31 @@
-# Import the required moduls
+# import the required moduls
 import os
 import functions
 
-# Start the loop to process the command of user
+# start the loop to process the command of user
 while True:
-	# Ask the user what to do
+	# ask the user what to do
 	command = input("What do you want to do? > ").lower()
 
-	# If command is same as done do this
+	# exit the program
 	if command == 'done' or command == 'd':
 		break
 
-	# If command is same as help do this
+	# print out the help function
 	elif command == 'help' or command == 'h':
 		functions.show_help()
 		continue
 
-	# If command is same as new table do this
+	# creates a new table in the database
 	elif command == 'new table' or command == 'ntb':
 		functions.creat_table()
 		continue
 
-	# If command is same as new do this
+	# create a new person
 	elif command == 'new' or command == 'n':
 		functions.new_person()
 		continue
 
-	# If command is same as show do this
-	elif command == 'show' or command == 's':
-		show_persons()
-		continue
-
-	# If command is not equal to anything than do this
+	# Is the command is not legit print this
 	else:
 		print('This is not a legit command')
--- functions.py
+++ functions.py
@@ -1,6 +1,23 @@
+################ ToDo's #################
+# @ToDo Add comments to the code in functions.py
+# @ToDo Add clear statments in code (functions)
+## @ToDoCreat ToDo function
+
+
+# Import the required moduls
 import mysql.connector
+import os
+import datetime
+
+def clear_screen():
+	# if operation systeem is Windows turn on this one
+	os.system('cls')
+
+	# if operationsysteem is OS turn on this one
+	# os.system('clear')
 
 def show_help():
+	# Prints out the commands that the user can use
 	print("Enter 'DONE' if you are finished")
 	print("Enter 'HELP' for help")
 	print("Enter 'NDB' for a new database")
@@ -24,10 +41,10 @@ def creat_table():
 	db = login()
 	cursor = db.cursor()
 
-	# Ask the user what the name of the table
+	# ask the user what the name of the table
 	name_new_table = input("What is the name of the new table? > ").lower()
 
-	# Create a new table 
+	# create a new table 
 	sql = """CREATE TABLE """ + name_new_table + """ ( 
 		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 		firstname VARCHAR(30) NOT NULL,
@@ -41,12 +58,12 @@ def creat_table():
 	);"""
 
 	try:
-		# Execute the SQL command
+		# execute the SQL command
 		cursor.execute(sql)
-		# Commit your changes in the database
+		# commit your changes in the database
 		db.commit()
 	except:
-		# Rollback in case there is any error
+		# rollback in case there is any error
 		db.rollback()
 		print('aborted')
 
@@ -54,10 +71,7 @@ def creat_table():
 	db.close()
 
 def new_person():
-	# login to the server
-	db = login()
-	cursor = db.cursor()
-
+	# ask the user his/her first name
 	firstname = input("What is the firstname? ").capitalize()
 	
 	check_mid_name = True
@@ -73,6 +87,8 @@ def new_person():
 
 	lastname = input("What is the lastname? ").capitalize()
 	
+	clear_screen()
+
 	loop_phonenumber = True
 
 	while loop_phonenumber:
@@ -85,14 +101,74 @@ def new_person():
 
 	print("If you are a male type 'm'")
 	print("If you are a female type 'f'")
-	print("If you don't know than type 'ng'")
-	gender = input("What is your gender? ")
 
 	while True:
-		if len(gender) == 1 or gender == 'ng':
-			if gender == 'ng':
-				print('LOL')
-		else:
+		gender = input("What is your gender? ")
+		if gender == 'gn':
+			print('LOL')
+			break
+		elif len(gender) >= 2:
 			print('You didnt read the discription. Will go faster if you do!')
+		else:
+			break
 
+	print('For the next three questions use numbers.')
+	while True:
+		birthday = input('What is your day of birth? ')
+		check_birthday = birthday.isdigit()
+		if check_birthday is False:
+			clear_screen()
+			print('That is not a number!')
+		elif len(birthday) >= 3:
+			clear_screen()
+			print("Your use more than 2 numbers.")
+			print("Please use 2 numbers.")
+		elif len(birthday) <= 1:
+			clear_screen()
+			print("You used less than 2 numbers.")
+			print("Please use 2 numbers.")
+		elif int(birthday) >= 31 or int(birthday) <= 0:
+			clear_screen()
+			print("Every month has 30 or 31 days not " + birthday + " days")
+		else:
+			break
 
+	while True:
+		month_of_birth = input('What is the month you were born in? ')
+		check_month_of_birth = month_of_birth.isdigit()
+		if check_month_of_birth is False:
+			clear_screen()
+			print('That is not a number!')
+		elif len(month_of_birth) >= 3:
+			clear_screen()
+			print("Your use more than 2 numbers.")
+			print("Please use 2 numbers.")
+		elif len(month_of_birth) <= 1:
+			clear_screen()
+			print("You used less than 2 numbers.")
+			print("Please use 2 numbers.")
+		elif int(month_of_birth) >= 31 or int(month_of_birth) <= 0:
+			clear_screen()
+			print("Every year has 12 months not " + month_of_birth + " months")
+		else:
+			break
+
+	while True:
+		year_of_birth = input('What is the year you were born in? (4 numbers)')
+		check_year_of_birth = year_of_birth.isdigit()
+		now = datetime.datetime.now()
+		if check_year_of_birth is False:
+			clear_screen()
+			print('That is not a number')
+		elif len(year_of_birth) >= 5:
+			clear_screen()
+			print('You used more than 4 numbers.')
+			print('Please use 4 numbers')
+		elif len(year_of_birth) <= 3:
+			clear_screen()
+			print('You used less than 4 numbers.')
+			print('Please use 4 nubmers')
+		elif int(year_of_birth) > now.year:
+			print('We are not in that year jet')
+		else:
+			break
